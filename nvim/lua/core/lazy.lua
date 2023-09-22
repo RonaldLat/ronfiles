a1d5306117d861876036286ca6f910683377cf4e
Ronald Otieno <ronnlats@gmail.com>
2023-09-22 11:36:03 +0000

    new colorschemes

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -180,6 +180,17 @@ lazy.setup({
 
 { 'sainnhe/gruvbox-material'},
 {'jnurmine/Zenburn'},
+{'luisiacc/gruvbox-baby'},
+{
+  'uloco/bluloco.nvim',
+  lazy = false,
+  priority = 1000,
+  dependencies = { 'rktjmp/lush.nvim' },
+  config = function()
+    -- your optional config goes here, see below.
+  end,
+},
+
 
 {'FrenzyExists/aquarium-vim'},
 {
