262512a73636401888738032d3fcd5fef2158870
Ronald Otieno <ronnlats@gmail.com>
2024-06-01 19:23:43 +0300

    xxx

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				require("none-ls.diagnostics.eslint_d"),
-				-- null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+    null_ls.setup({
+      sources = {
+        -- require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
+  end,
 }
